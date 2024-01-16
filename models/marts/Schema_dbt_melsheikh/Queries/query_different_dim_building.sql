048338331b96d4ec33b95f4ceac8025a125c3915
Mohamed Elsheikh <mohamed2016170329@cis.asu.edu.eg>
2024-01-16 07:16:03 +0000

    Query to Count the number of buildings by different dimensions

---
+++ models/marts/Schema_dbt_melsheikh/Queries/query_different_dim_building.sql
@@ -0,0 +1,50 @@
+with 
+
+Location_Dim as (
+
+    select
+    
+        *
+
+    from DIM_LOCATION
+),
+
+
+borough_num as (
+    select
+
+        count(borough_name) num_building_borough,
+        concat('Borough: ', borough_name) as  borough_name_idn
+
+    from Location_Dim
+
+    group by borough_name
+),
+
+
+neighbourhood_num as (
+    select
+
+        count(Neighborhood) num_building_neighbourhood,
+        concat('Neighborhood: ', Neighborhood) as  Neighborhood_idn
+        
+
+    from Location_Dim
+
+    group by Neighborhood
+),
+
+
+show_data as (
+    select
+        *
+    from borough_num
+
+    union
+
+    select
+        *
+    from neighbourhood_num
+)
+
+select * from show_data
