5f86f6baf75cca2d413d237a8bdad65686d7267b
Mohamed Elsheikh <mohamed2016170329@cis.asu.edu.eg>
2024-01-25 15:39:28 +0000

    update date relations (ship - order) of fact (sql - yml)

--- models/DWH/schema_superstore/Facts/fact_orders.sql
+++ models/DWH/schema_superstore/Facts/fact_orders.sql
@@ -41,6 +41,17 @@ dim_product_cte as (
 ),
 
 
+dim_calendar_date_cte as (
+
+    select 
+
+        *
+    
+    from {{ ref('dim_calendar_date') }}
+
+),
+
+
 fact_orders as (
 
     select
@@ -53,9 +64,9 @@ fact_orders as (
         dim_product_cte.product_id,
         dim_location_cte.location_id,
 
-        -- generate dates foriegn keys 
-        to_number(to_varchar(order_date, 'YYYYMMDD')) as date_id_order,
-        to_number(to_varchar(ship_date, 'YYYYMMDD')) as date_id_ship,
+        -- dates foriegn keys 
+        dim_calendar_date_order.date_id as order_date_id,
+        dim_calendar_date_ship.date_id as ship_date_id,
 
         -- degenerated dimension
         ship_mode as dd_ship_mode,
@@ -68,12 +79,28 @@ fact_orders as (
     
     from source
 
+
+    -- join customer dimension
     left join dim_customer_cte on
         dim_customer_cte.customer_id = source.customer_id
 
+
+    -- join product dimension
     left join dim_product_cte on
         dim_product_cte.product_id = source.product_id
 
+
+    -- join order date in calendar dimension
+    left join dim_calendar_date_cte as dim_calendar_date_order on
+        dim_calendar_date_order.main_date = source.order_date
+
+
+    -- join ship date in calendar dimension
+    left join dim_calendar_date_cte as dim_calendar_date_ship on
+        dim_calendar_date_ship.main_date = source.ship_date
+
+
+    -- join location dimension
     left join dim_location_cte on
         source.country = dim_location_cte.location_country and
         source.city = dim_location_cte.location_city and
