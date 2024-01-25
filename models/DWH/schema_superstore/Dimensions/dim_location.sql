917696d685690d5a9ce595a3baefa110792a9012
Mohamed Elsheikh <mohamed2016170329@cis.asu.edu.eg>
2024-01-25 09:50:46 +0000

    create location dimension table and add tests in yml file

---
+++ models/DWH/schema_superstore/Dimensions/dim_location.sql
@@ -0,0 +1,36 @@
+with
+
+source as (
+
+    select
+        
+        *
+        
+    from {{ ref('stg_schema_superstore__superstore_sample') }}
+),
+
+location_set as (
+
+    select distinct
+
+        row_number() over (order by (select null)) as location_id,
+        country as location_country,
+        city as location_city,
+        state as location_state,
+        postal_code as location_postal_code,
+        region as location_region
+        
+    from source
+
+),
+
+dim_location as (
+
+    select
+        
+        *
+    
+    from location_set
+)
+
+select * from dim_location
